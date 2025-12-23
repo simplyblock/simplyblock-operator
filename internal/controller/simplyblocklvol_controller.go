2a9931b2c96f12b8937e65200c4e2834eaa9743c
geoffrey1330 <israelgeoffrey13@gmail.com>
2025-12-23 11:19:49 +0100

    added lvol api spec and status

---
+++ internal/controller/simplyblocklvol_controller.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	simplyblockv1alpha1 "github.com/simplyblock/simplyblock-manager/api/v1alpha1"
+)
+
+// SimplyBlockLvolReconciler reconciles a SimplyBlockLvol object
+type SimplyBlockLvolReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=simplyblock.simplyblock.io,resources=simplyblocklvols,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=simplyblock.simplyblock.io,resources=simplyblocklvols/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=simplyblock.simplyblock.io,resources=simplyblocklvols/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the SimplyBlockLvol object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.22.4/pkg/reconcile
+func (r *SimplyBlockLvolReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = logf.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *SimplyBlockLvolReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&simplyblockv1alpha1.SimplyBlockLvol{}).
+		Named("simplyblocklvol").
+		Complete(r)
+}
