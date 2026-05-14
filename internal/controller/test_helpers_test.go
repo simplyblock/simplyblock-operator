454e16864161b6d562bcc65f294312eb9b2f9c4a
Manohar Reddy <manohar@simplyblock.io>
2026-05-14 05:17:18 +0200

    remove CRDs for Device and Lvol

--- internal/controller/test_helpers_test.go
+++ internal/controller/test_helpers_test.go
@@ -3,9 +3,13 @@ package controller
 import (
 	"testing"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	simplyblockv1alpha1 "github.com/simplyblock/simplyblock-operator/api/v1alpha1"
 )
 
 func newTestScheme(t *testing.T, addToScheme ...func(*runtime.Scheme) error) *runtime.Scheme {
@@ -47,4 +51,30 @@ func contains(items []string, want string) bool {
 		}
 	}
 	return false
+}
+
+func testCluster(namespace, clusterName, uuid string) *simplyblockv1alpha1.StorageCluster {
+	return &simplyblockv1alpha1.StorageCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      clusterName,
+			Namespace: namespace,
+		},
+		Spec: simplyblockv1alpha1.StorageClusterSpec{},
+		Status: simplyblockv1alpha1.StorageClusterStatus{
+			UUID: uuid,
+		},
+	}
+}
+
+func testClusterSecret(namespace, clusterName, uuid, secret string) *corev1.Secret {
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "simplyblock-cluster-" + clusterName,
+			Namespace: namespace,
+		},
+		Data: map[string][]byte{
+			"uuid":   []byte(uuid),
+			"secret": []byte(secret),
+		},
+	}
 }
--- internal/utils/constants.go
+++ internal/utils/constants.go
@@ -2,9 +2,7 @@ package utils
 
 const (
 	FinalizerPool                = "storage.simplyblock.io/pool-finalizer"
-	FinalizerLvol                = "storage.simplyblock.io/lvol-finalizer"
 	FinalizerTask                = "storage.simplyblock.io/task-finalizer"
-	FinalizerDevice              = "storage.simplyblock.io/device-finalizer"
 	FinalizerSnapshotReplication = "storage.simplyblock.io/snapshotreplication-finalizer"
 	FinalizerStorageNode         = "storage.simplyblock.io/storagenode-finalizer"
 	FinalizerStorageCluster      = "storage.simplyblock.io/cluster-finalizer"
@@ -39,12 +37,6 @@ const (
 	NodeStatusOffline     = "offline"
 	NodeStatusInRestart   = "in_restart"
 	NodeStatusUnreachable = "unreachable"
-
-	DeviceActionRemove  = "remove"
-	DeviceActionRestart = "restart"
-
-	DeviceStatusRemoved = "removed"
-	DeviceStatusOnline  = "online"
 
 	ENDPOINT       = "http://simplyblock-webappapi:5000"
 	CSIProvisioner = "csi.simplyblock.io"
