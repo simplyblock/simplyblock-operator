cefd7410eff4a95eb2cabb3f9d819417e0325589
Max Schettler <max@simplyblock.io>
2026-06-15 17:05:14 +0200

    Remove left-over cluster secret references

--- internal/controller/simplyblockstoragecluster_controller.go
+++ internal/controller/simplyblockstoragecluster_controller.go
@@ -263,22 +263,6 @@ func (r *StorageClusterReconciler) reconcileCreate(
 	}
 
 	log.Info("Cluster successfully created", "name", clusterCR.Name)
-
-	// clusterUUID, clusterSecret, err := utils.GetClusterAuth(ctx, r.Client, clusterCR.Namespace, clusterCR.Name)
-	// if err != nil {
-	// 	log.Error(err, "Failed to get cluster auth")
-	// 	return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
-	// }
-
-	// endpoint := fmt.Sprintf("/api/v2/clusters/%s/update", clusterUUID)
-
-	// body, status, err := apiClient.Do(ctx, http.MethodPost, endpoint, updateParams)
-	// if err != nil || status >= 300 {
-	// 	log.Error(err, "Cluster update failed", "status", status, "response", string(body))
-	// 	return ctrl.Result{RequeueAfter: 20 * time.Second}, nil
-	// }
-
-	// log.Info("Cluster updated successfully", "name", clusterCR.Name)
 	return ctrl.Result{}, nil
 }
 
@@ -414,11 +398,6 @@ func (r *StorageClusterReconciler) handleDeletion(
 		return ctrl.Result{RequeueAfter: 10 * time.Second}, true, nil
 	}
 
-	if err := r.deleteClusterSecret(ctx, clusterCR); err != nil {
-		log.Error(err, "Failed to delete cluster secret, will retry", "name", clusterCR.Name)
-		return ctrl.Result{RequeueAfter: 10 * time.Second}, true, nil
-	}
-
 	controllerutil.RemoveFinalizer(clusterCR, utils.FinalizerStorageCluster)
 	return ctrl.Result{}, true, r.Update(ctx, clusterCR)
 }
@@ -434,32 +413,6 @@ func (r *StorageClusterReconciler) ensureFinalizer(
 
 	controllerutil.AddFinalizer(clusterCR, utils.FinalizerStorageCluster)
 	return true, r.Update(ctx, clusterCR)
-}
-
-func (r *StorageClusterReconciler) deleteClusterSecret(
-	ctx context.Context,
-	clusterCR *simplyblockv1alpha1.StorageCluster,
-) error {
-
-	secretName := clusterCR.Status.SecretName
-	if secretName == "" {
-		secretName = fmt.Sprintf("simplyblock-cluster-%s", clusterCR.Name)
-	}
-
-	secret := &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      secretName,
-			Namespace: clusterCR.Namespace,
-		},
-	}
-
-	if err := r.Delete(ctx, secret); err != nil {
-		if client.IgnoreNotFound(err) != nil {
-			return err
-		}
-	}
-
-	return nil
 }
 
 func (r *StorageClusterReconciler) reconcileActivate(
